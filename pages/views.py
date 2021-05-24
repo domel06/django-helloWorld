7e8c20e11b92bc13bc20aea9b254c77e2e3ede84
domelBura <burajanuary@gmail.com>
2021-05-24 22:56:02 +0300

    first django

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+def homePageView(request): 
+    return HttpResponse('Hello, World!')
+
+    
+
+# Create your views here.
